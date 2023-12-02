5d6d869cc83e84b4407b3ea8f6eefd695c943416
Prince Hirpara <pd@princes-macbook-pro.local>
2023-12-02 20:52:31 +0530

    Second Commit

--- android/app/src/main/kotlin/com/example/contect_diary_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/contect_diary_1/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.contect_diary_1
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
